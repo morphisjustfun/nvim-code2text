39725428671d75cca936956186da1f16a86a0602
lordmarcusvane <mariojacobo2011@gmail.com>
2021-10-09 07:37:42 -0500

    Refactoring, guards and cleanup

--- lua/nvim-code2text/init.lua
+++ lua/nvim-code2text/init.lua
@@ -14,7 +14,7 @@ local resultText = ""
 -- Original window in which enterCode has been called
 local currentWindow = nil
 -- Splitted buffer created when enterCode is called
-local newBuf = nil
+local newSplittedBuffer = nil
 
 local function enterCode(language)
 	if not utils.contains(languagesAvailable, language) then
@@ -28,43 +28,46 @@ local function enterCode(language)
 		vim.api.nvim_command("botright vsplit")
 		local splittedWindow = vim.api.nvim_get_current_win()
 
-		if newBuf == nil then
-			newBuf = vim.api.nvim_create_buf(true, true)
+		if newSplittedBuffer == nil then
+			newSplittedBuffer = vim.api.nvim_create_buf(true, true)
 		end
 
 		if language == nil then
-			vim.api.nvim_buf_set_name(newBuf, "Code2Text")
+			vim.api.nvim_buf_set_name(newSplittedBuffer, "Code2Text")
 		else
-			vim.api.nvim_buf_set_name(newBuf, "Code2Text")
+			vim.api.nvim_buf_set_name(newSplittedBuffer, "Code2Text")
 		end
 
-		vim.api.nvim_buf_set_option(newBuf, "filetype", languageSelected)
-		vim.api.nvim_buf_set_option(newBuf, "swapfile", false)
+		vim.api.nvim_buf_set_option(newSplittedBuffer, "filetype", languageSelected)
+		vim.api.nvim_buf_set_option(newSplittedBuffer, "swapfile", false)
 
-		vim.api.nvim_win_set_buf(splittedWindow, newBuf)
+		vim.api.nvim_win_set_buf(splittedWindow, newSplittedBuffer)
 	end
 end
 
 local function getOutput()
-	resultText = vim.api.nvim_exec("w !" .. languagesExecutable[languageSelected], true)
-	print(resultText)
-end
-
-local function getCurrentLine(window)
-	vim.api.nvim_set_current_win(window)
-	return vim.api.nvim_exec('echo line(".")', true)
+	if currentWindow == nil or vim.fn.bufname() ~= "Code2Text" then
+		print("Code2Text buffer is not open/focused")
+	else
+		resultText = vim.api.nvim_exec("w !" .. languagesExecutable[languageSelected], true)
+		print(resultText)
+	end
 end
 
 local function acceptOutput()
-	resultText = vim.api.nvim_exec("w !" .. languagesExecutable[languageSelected], true)
-	vim.api.nvim_command("q")
-	currentLine = getCurrentLine(currentWindow)
-	currentLine = tonumber(currentLine)
-	-- Taken from https://gist.github.com/iwanbk/5479582
-	for line in resultText:gmatch("([^\n]*)\n?") do
-		if line ~= "" then
-			vim.api.nvim_command("put='" .. line .. "'")
+	if currentWindow == nil or vim.fn.bufname() ~= "Code2Text" then
+		print("Code2Text buffer is not open/focused")
+	else
+		resultText = vim.api.nvim_exec("w !" .. languagesExecutable[languageSelected], true)
+		vim.api.nvim_command("q")
+		vim.api.nvim_set_current_win(currentWindow)
+		-- Taken from https://gist.github.com/iwanbk/5479582
+		for line in resultText:gmatch("([^\n]*)\n?") do
+			if line ~= "" then
+				vim.api.nvim_command("put='" .. line .. "'")
+			end
 		end
+		currentWindow = nil
 	end
 end
 
